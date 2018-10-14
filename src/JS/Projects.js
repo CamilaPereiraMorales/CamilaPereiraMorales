dc197859bee2fca20705144f4b62883615ad1fdd
Camila <camipereira2111@gmail.com>
2018-10-14 16:31:11 -0300

    redireccionar desde Proyectos

---
+++ src/JS/Projects.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+function redirectToIndex() {
+    location = "/src/HTML/index.html"
+}
+
+function redirectToAboutMe() {
+    location = "/src/HTML/AboutMe.html"
+}
