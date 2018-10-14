c052a57f2417eed5320e1946f2982aa12f4d3bc7
Camila <camipereira2111@gmail.com>
2018-10-14 16:31:25 -0300

    redireccionar desde About me

---
+++ src/JS/AboutMe.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+function redirectToProjects() {
+    location = "/src/HTML/Projects.html"
+}
+
+function redirectToIndex() {
+    location = "/src/HTML/index.html"
+}
