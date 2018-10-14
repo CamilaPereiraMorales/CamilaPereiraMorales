9cbc3700acb9e3c88ffc36e1b86cf09b03ba1484
Camila <camipereira2111@gmail.com>
2018-10-14 19:26:24 -0300

    img Hover puesto, background blanco,

--- src/JS/Projects.js
+++ src/JS/Projects.js
@@ -9,3 +9,16 @@ function redirectToIndex() {
 function redirectToAboutMe() {
     location = "/src/HTML/AboutMe.html"
 }
+/* Demo purposes only */
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+    snippet.forEach(function(snippet) {
+        snippet.addEventListener('mouseout', function(event) {
+            if (event.target.parentNode.tagName === 'figure') {
+                event.target.parentNode.classList.remove('hover')
+            } else {
+                event.target.parentNode.classList.remove('hover')
+            }
+        });
+    });
+}
