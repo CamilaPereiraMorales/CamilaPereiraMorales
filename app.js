91eccd1fe3dd632a967772f516deeb1f020c5faf
Camili <camipereira2111@gmail.com>
2019-10-01 15:33:19 -0300

    estilo e imagen nueva

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 new fullpage('#fullpage', {
-  sectionsColor: ['#889BB7', 'rgb(78, 83, 88)', '#C0C0C0', '#ADD8E6'],
+  sectionsColor: ['#EEE2DC', '#BAB2B5', '#C0C0C0', '#ADD8E6'],
 });
