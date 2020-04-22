f554a8e61feb608a96f7a86264d33bbef0313003
Camili <camipereira2111@gmail.com>
2020-04-22 12:19:24 -0400

    ediciones en mobile

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ $(document).ready(function(){
 });
 new fullpage('#fullpage', {
   sectionsColor: ['#EEE2DC', '#BAB2B5', '#C0C0C0', '#ADD8E6'],
+  menu: '#myNavbar',
+  anchors: ['process1', 'process2', 'process3', 'process4'],
 });
